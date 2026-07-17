054b3e725f69889582e6315f400ecf2f58bb1905
avxtecnologia <avexadostecnologia@gmail.com>
2026-07-17 01:47:27 -0300

    Remove background class from main div

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ function StoreContent() {
   }
 
   return (
-    <div className="min-h-dvh bg-background text-foreground">
+    <div className="min-h-dvh text-foreground">
       <TopBar />
 
       <main className="mx-auto max-w-7xl px-4 pb-32 pt-6 sm:px-6">
